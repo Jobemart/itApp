1e6d696a813b736c22ad0d6d84777b8e80ad8f8c
Jobemart <joan.benito.martinez@gmail.com>
2016-11-22 13:23:05 +0100

    Added controllers

---
+++ itApp/Controllers/DataController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data.Common;
+using System.Data.OleDb;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace itApp.Controllers
+{
+    public class DataController : Controller
+    {
+        private static DbConnect conn;
+
+        public DataController(){
+            
+        }
+
+        public void DataBaseConnection()
+        {
+            int id = 123;
+            String title = "aa";
+
+            try
+            {
+                conn = new DbConnect();
+                conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;"
+                    +"Data source= C:/Users/j.benito.DPIDC/Documents/Visual Studio 2015/Projects/BBDD.accdb";
+
+                DbCommand cmd = conn.CreateCommand();
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "INSERT INTO Prueba ([ID],[Title]) VALUES(@ID, @Title)";    
+                
+                DbParam idParam = new DbParam();
+                idParam.ParameterName = "@ID";
+                idParam.Value = id;
+
+                DbParam titleParam = new DbParam();
+                idParam.ParameterName = "@Title";
+                idParam.Value = title;
+
+                cmd.Parameters.Add(idParam);
+                cmd.Parameters.Add(titleParam);
+
+                cmd.Connection = conn;
+
+                conn.Open();
+                // Insert code to process data.
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to connect to data source");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+    }
+
+}
